039833a1b979d9c591edec6df83b4fdb4e51525f
James Layton <jameshanxinsun@gmail.com>
2018-04-18 16:36:33 -0400

    - Added new classes and rearranged files

---
+++ StarWarsFun/StarWarsFun/Controller/DetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailViewController.swift
+//  StarWarsFun
+//
+//  Created by James H Layton on 4/18/18.
+//  Copyright © 2018 james. All rights reserved.
+//
+
+import Foundation
---
+++ StarWarsFun/StarWarsFun/Model/SegueData.swift
@@ -0,0 +1,9 @@
+//
+//  SegueData.swift
+//  StarWarsFun
+//
+//  Created by James H Layton on 4/18/18.
+//  Copyright © 2018 james. All rights reserved.
+//
+
+import Foundation
